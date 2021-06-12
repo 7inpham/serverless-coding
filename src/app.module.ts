a47fd6ab76d111a78fc230f45c242bb6170792d4
Tin Pham <7inpham@gmail.com>
2021-06-12 22:21:40 +0700

    Allow creation of user

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ArticlesModule } from './articles/articles.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [ArticlesModule],
+  imports: [ArticlesModule, UsersModule],
   controllers: [],
   providers: [],
 })
